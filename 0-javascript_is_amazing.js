bdc45a0efb927a39cfd0fc6c95ca25d5e72963f8
nimivera <etimv0524@gmail.com@example.com>
2025-05-22 11:21:49 +0000

    Add  0-javascript_is_amazing.js script

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,3 +1,4 @@
 const myVar = "JavaScript is amazing";
 console.log(myVar);
-// Output: JavaScript is amazing
+// Output: JavaScript is amazionst myVar = "JavaScript is amazing";
+console.log(myVar);ng
